62cec5dfde932aa700f02328dacce0b9167bb861
Quinn Wienke <qwienke13@gmail.com>
2025-02-03 20:35:14 -0700

    Added search function and new page to view national dex

---
+++ New pokedex/Views/Team/AddTeamView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTeamView.swift
+//  New pokedex
+//
+//  Created by Quinn Wienke on 2/3/25.
+//
+
+import SwiftUI
+
+struct AddTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTeamView()
+}
